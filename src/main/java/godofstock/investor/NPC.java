672141795fcd1fa09728f51ca7b9ab26ef48af72
Yu seungbeom <dldkdlelqnzo@gmail.com>
2024-05-20 16:39:32 +0900

    feat: 메인 화면 텍스트 구성 완료

--- src/main/java/godofstock/investor/NPC.java
+++ src/main/java/godofstock/investor/NPC.java
@@ -5,8 +5,19 @@ import java.util.Random;
 import static godofstock.company.Company.NUMBER_OF_COMPANIES;
 
 public abstract class NPC extends Investor {
+    private int investAmount;
+
+    public int getInvestAmount() {
+        return investAmount;
+    }
+
     @Override
     public int choiceCompany() {
         return new Random().nextInt(NUMBER_OF_COMPANIES) + 1;
+    }
+
+    int calculate(double percentage) {
+        investAmount = (int) (getBudget() * percentage);
+        return investAmount;
     }
 }
